8a375eafcc5ae6446a4467b2d5acfc461dab3c6a
umeshkumar1991 <umesh.kumar@tothenew.com>
2017-06-15 00:16:43 +0530

    step1: basic greet method()

---
+++ src/ttn/kssJava8/session1/Greeter.java
@@ -0,0 +1,14 @@
+package ttn.kssJava8.session1;
+
+public class Greeter {
+
+    public void greet() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.greet();
+    }
+
+}
