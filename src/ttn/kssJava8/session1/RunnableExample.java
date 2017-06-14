91576bd2bfb7ab2b25b488b4767802969055c74d
umeshkumar1991 <umesh.kumar@tothenew.com>
2017-06-15 01:53:38 +0530

    step 4: type inference and runnable exaple

---
+++ src/ttn/kssJava8/session1/RunnableExample.java
@@ -0,0 +1,20 @@
+package ttn.kssJava8.session1;
+
+
+public class RunnableExample {
+
+    public static void main(String[] args) {
+
+        Thread myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Printed inside Runnable");
+            }
+        });
+        myThread.start();
+
+        Thread myLambdaThread = new Thread(() -> System.out.println("Printed inside Runnable using Lambda"));
+    }
+
+
+}
---
+++ src/ttn/kssJava8/session1/TypeInferenceExample.java
@@ -0,0 +1,30 @@
+package ttn.kssJava8.session1;
+
+
+public class TypeInferenceExample {
+
+    public static void main(String[] args) {
+        StringLengthLambda myLambda = (String s) -> s.length();
+        System.out.println(myLambda.length("Hello World"));
+
+        //Todo:1
+        //  StringLengthLambda myLambda = s -> s.length();
+
+
+        //Todo:2
+        //printLambda(s -> s.length());
+
+    }
+
+
+    interface StringLengthLambda {
+        int length(String s);
+    }
+
+
+    public static void printLambda(StringLengthLambda l) {
+        System.out.println(l.length("Hello World"));
+    }
+}
+
+
